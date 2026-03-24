ea955c0feb9c65902d32ed7985288890deea1d13
Abiria <oro3673@gmail.com>
2026-03-24 03:59:23 -0400

    response schema factory

--- apps/api/src/schemas.ts
+++ apps/api/src/schemas.ts
@@ -1,4 +1,5 @@
 import { z } from '@hono/zod-openapi'
+import type { ZodSchema } from 'zod/v3'
 
 // zod v4 codec
 // https://zod.dev/codecs?id=isodatetimetodate
@@ -6,3 +7,16 @@ export const isoDatetimeToDate = z.codec(z.iso.datetime(), z.date(), {
 	decode: isoString => new Date(isoString),
 	encode: date => date.toISOString(),
 })
+
+export const apiResponseSchemaFactory = (schema: ZodSchema) =>
+	z.union([
+		z.object({
+			ok: true,
+			message: z.string(),
+			data: schema,
+		}),
+		z.object({
+			ok: false,
+			message: z.string(),
+		}),
+	])
