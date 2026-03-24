0a665b9414a2fbeb35d6fa250fd7b9fd0e89569a
Abiria <oro3673@gmail.com>
2026-03-24 03:59:05 -0400

    handler for 404 and error in /api

--- apps/api/src/api.ts
+++ apps/api/src/api.ts
@@ -2,8 +2,22 @@ import { OpenAPIHono } from '@hono/zod-openapi'
 import type { InjectedEnv } from './env'
 import { app as pasteApp } from './paste.controller'
 import { app as userApp } from './user.controller'
+import { HTTPException } from 'hono/http-exception'
 
 export const app = new OpenAPIHono<InjectedEnv>()
 
 app.route('/pastes', pasteApp)
 app.route('/users', userApp)
+
+app.onError((err, c) => {
+	if (err instanceof HTTPException) {
+		return c.json({ ok: false, message: err.message }, err.status)
+	}
+
+	return c.json({ ok: false, message: err.message }, 500)
+})
+
+// https://github.com/honojs/hono/issues/3465
+app.all('*', c => {
+	return c.json({ ok: false, message: 'Not found' }, 404)
+})
