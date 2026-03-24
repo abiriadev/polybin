81d104db9b39464cfcd37d51c11be78e5918f1ab
Abiria <oro3673@gmail.com>
2026-03-23 23:09:18 -0400

    schema for users

---
+++ apps/api/src/user.schema.ts
@@ -0,0 +1,16 @@
+import z from 'zod'
+import { isoDatetimeToDate } from './schemas'
+
+export const userBaseSchema = z.object({
+	id: z.string(),
+	name: z.string(),
+	createdAt: isoDatetimeToDate,
+})
+
+export type UserBase = z.infer<typeof userBaseSchema>
+
+export const userNewSchem = userBaseSchema
+	.omit({ id: true, createdAt: true })
+	.openapi('UserNew')
+
+export type UserNew = z.infer<typeof userNewSchem>
